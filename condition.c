3a7a1eb4b4eced40d6c6a090e8a16564d4960dfa
k2h2j3 <74819625+k2h2j3@users.noreply.github.com>
2022-07-01 17:42:27 +0900

    Add files via upload

--- condition.c
+++ condition.c
@@ -76,6 +76,48 @@ int main(void)
 	//	printf("오류!\n");
 	//	break;
 	//}
+	
+	
+	//숫자 맞추기 프로젝트
+
+	srand(time(NULL));
+	int num = rand() % 100 + 1; // 1 ~ 100 사이의 숫자 랜덤
+	printf("숫자: %d\n", num);
+	int answer = 0; //정답
+	int chance = 5; //기회
+	while (chance > 0)
+	{
+		printf("남은 기회 %d 번\n", chance--);
+		printf("숫자를 맞혀보세요 1 ~ 100\n");
+		scanf_s("%d", &answer);
+
+		if (answer > num)
+		{
+			printf("DOWN \n\n");
+
+		}
+		else if(answer < num)
+		{
+			printf("UP\n\n");
+		}
+		else if (answer = num)
+		{
+			printf("정답!\n\n");
+			break;
+		}
+		else
+		{
+			printf("1 ~ 100사이의 숫자만 입력해주세요\n");
+			break;
+		}
+
+		if (chance == 0)
+		{
+			printf("남은 기회를 모두 소진했습니다.\n");
+			break;
+		}
+	}
+
 
 
 
