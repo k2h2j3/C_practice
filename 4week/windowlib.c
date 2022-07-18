f729ae2613cafcaeac36cd97e6e6e4cf6ad39871
k2h2j3 <74819625+k2h2j3@users.noreply.github.com>
2022-07-18 18:18:25 +0900

    Add files via upload

--- 4week/windowlib.c
+++ 4week/windowlib.c
@@ -3,13 +3,23 @@
 
 void gotoxy(int x, int y);
 
-int main_window(void)
+int main(void)
 {
-	int x, y;
+	/*int x, y;
 	printf("문자열을 띄울 위치를 정해주세요(x,y) :" );
 	scanf_s("%d %d", &x, &y);
 	gotoxy(x, y);
-	printf("Hello World!");
+	printf("Hello World!");*/
+
+	//Sleep 함수 => 지연시간을 두는 함수
+	int i;
+	int time = 1000;
+	for (i = 0; i < 10; i++) 
+	{
+		printf("%d초 마다 출력\n", time / 1000);
+		Sleep(time);
+	}
+
 
 	return 0;
 }
@@ -20,3 +30,5 @@ void gotoxy(int x, int y)
 	COORD pos = { x,y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
+
+
